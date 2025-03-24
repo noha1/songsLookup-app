c78f2bae190fdfe8946b819e871a9238d9c328c1
noha <noha00@hotmail.com>
2025-03-24 19:58:21 +0000

    fix env

--- server/server.js
+++ server/server.js
@@ -5,11 +5,12 @@ const port = process.env.PORT || 5000;
 const axios = require("axios");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const MUSIXMATCH_API_KEY = process.env.MUSIXMATCH_API_KEY;
+const HUGGINGFACE_API_TOKEN= process.env.HUGGINGFACE_API_TOKEN;
 
 app.use(cors());
 app.use(bodyParser.json());
 
-const MUSIXMATCH_API_KEY = "832b4eaaee025e91b12c7ec4894e8eba";
 // mock login credentials 
 //const users = [{ email: "test@hotmail.com", password: "password123" }];
 
@@ -48,7 +49,7 @@ app.get("/track",async (req, res) => {
 		trackResponse.data.message.body.track_list[0].track.track_id;
 		// Fetch lyrics for the song
 		// Check if the response is successful (e.g., status code is 200)
-		 if (trackResponse.status !== 200) {
+		 if (trackResponse.status !== 200 || trackId) {
 			throw new Error('Failed to process the song. Invalid response status.');
 		  }
 
@@ -83,7 +84,6 @@ app.get("/track",async (req, res) => {
 // Route to summarize song lyrics using ChatGPT
 app.post('/summarise', async (req, res) => {
   const { songLyrics } = req.body;
-  const apiKey ="sk-proj-UEJUbWF23vOZTGzSB5q6T3BlbkFJ7dJJuRl37aYdnaGh5VzZ";
 
   if (!songLyrics) {
     return res.status(400).json({ error: "No song lyrics provided" });
@@ -120,7 +120,7 @@ app.post('/summarise', async (req, res) => {
 const extractCountries = (lyrics) => {
 	const countries = [
 		  "USA", "Canada", "Germany", "France", "Brazil", 
-		  "India", "Mexico", "Italy", "Japan", "Spain", "Portugal", "UAE", ""
+		  "India", "Mexico", "Italy", "Japan", "Spain", "Portugal", "UAE", "UK"
 		];
 	  
 		// Create a hash set (using an object)
